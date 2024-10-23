f1044507ce4eaf23d695b1b7736b89ca4a1854a0
MrBrutti <matias@section9labs.com>
2024-10-23 09:56:10 -0700

    First tap

---
+++ Formula/filmlab.rb
@@ -0,0 +1,17 @@
+cask "filmlab" do
+  version "3.0.0"
+  sha256 :no_check
+
+  url "https://downloads.filmlabapp.com/desktop/FilmLab-3.0.0-universal.dmg"
+  name "FilmLab"
+  desc "Film photography scanning app"
+  homepage "https://www.filmlabapp.com/"
+
+  app "FilmLab.app"
+
+  zap trash: [
+    "~/Library/Application Support/FilmLab",
+    "~/Library/Preferences/com.filmlabapp.desktop.plist",
+    "~/Library/Saved Application State/com.filmlabapp.desktop.savedState",
+  ]
+end
