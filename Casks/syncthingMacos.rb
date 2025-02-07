78d172756430f6c7aef6f3751a009751fe7f787f
MrBrutti <matias@section9labs.com>
2025-02-06 17:25:10 -0800

    Adding syncthingsMacos casks, fixing formula error

--- Casks/syncthingMacos.rb
+++ Casks/syncthingMacos.rb
@@ -1,28 +1,16 @@
 
-class SyncthingMacos < Formula
-  desc "Native macOS packaging of Syncthing"
-  homepage "https://github.com/syncthing/syncthing-macos"
-  url "https://github.com/syncthing/syncthing-macos/releases/download/v1.29.2-1/Syncthing-1.29.2-1.dmg"
+cask "syncthing-macos" do
+  version "1.29.2-1"
   sha256 "90c8466ee81e6b3f87c97891e3488222e59c7983b7c7ec0fc86dfc2e18b644d768bceae91079f15b5653887d229705f47cf95d2741538376062f78dbaefe989b"
 
-  depends_on :macos
+  url "https://github.com/syncthing/syncthing-macos/releases/download/v#{version}/Syncthing-#{version}.dmg"
+  name "Syncthing for macOS"
+  desc "Native macOS packaging of Syncthing"
+  homepage "https://github.com/syncthing/syncthing-macos"
 
-  def install
-    system "hdiutil", "attach", "Syncthing-1.29.2-1.dmg"
-    system "cp", "-r", "/Volumes/Syncthing/Syncthing.app", prefix
-    system "hdiutil", "detach", "/Volumes/Syncthing"
-  end
+  app "Syncthing.app"
 
-  def caveats
-    <<~EOS
-      Syncthing-macos has been installed. You can launch it from:
-        #{prefix}/Syncthing.app
-      You may want to move it to /Applications manually.
-    EOS
-  end
-
-  test do
-    system "test", "-d", "#{prefix}/Syncthing.app"
+  caveats do
+    "You may want to move Syncthing.app to /Applications manually."
   end
 end
-
