feca674fcf52bd99730c04363eb8772887ea52c7
MrBrutti <matias@section9labs.com>
2025-02-06 17:31:20 -0800

    fixing hash

--- Casks/syncthing-macos.rb
+++ Casks/syncthing-macos.rb
@@ -1,7 +1,7 @@
 
 cask "syncthing-macos" do
   version "1.29.2-1"
-  sha256 "90c8466ee81e6b3f87c97891e3488222e59c7983b7c7ec0fc86dfc2e18b644d768bceae91079f15b5653887d229705f47cf95d2741538376062f78dbaefe989b"
+  sha256 "9b31bb415893e9a52250e6885d595c2e72de6c4ec9e0ad3f0ad01d172c1a585c"
 
   url "https://github.com/syncthing/syncthing-macos/releases/download/v#{version}/Syncthing-#{version}.dmg"
   name "Syncthing for macOS"
