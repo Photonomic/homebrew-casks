94da5c9762631aa0b8a24c9f7da11ce4d7c4bd41
MrBrutti <matias@section9labs.com>
2025-04-16 10:30:08 -0700

    Updating Capture One to latest version as of 4/16/25. Fixed URL error

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -3,7 +3,6 @@ cask "capture-one" do
   sha256 "94052a1f73bb73c512449c22513760578422ee77b800368346303eb2" # Update with the correct SHA256 if necessary
 
   url "https://downloads.captureone.pro/d/mac/0be2350895442f15d22e0696788bd7a8b925a511/CaptureOne.Mac.#{version}.dmg"
-  url "https://downloads.captureone.pro/d/mac/c84373c979835f35670946c106f170a9018ca0cc/CaptureOne.Mac.#{version}.dmg"
   name "Capture One"
   homepage "https://www.captureone.com/"
 
