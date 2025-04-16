75b7f57f79bc07213a94af1a6346a0ddf82f671b
MrBrutti <matias@section9labs.com>
2025-04-16 09:37:29 -0700

    Updating Capture One to latest version as of 4/16/25

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -1,7 +1,8 @@
 cask "capture-one" do
-  version "16.4.6.20"
-  sha256 "5896b4ce3f4142ec682bfa271a21ed305e11b0ebe755fb7e39f283fe5dbacf4f" # Update with the correct SHA256 if necessary
+  version "16.5.9.7"
+  sha256 "94052a1f73bb73c512449c22513760578422ee77b800368346303eb2" # Update with the correct SHA256 if necessary
 
+  url "https://downloads.captureone.pro/d/mac/0be2350895442f15d22e0696788bd7a8b925a511/CaptureOne.Mac.#{version}.dmg"
   url "https://downloads.captureone.pro/d/mac/c84373c979835f35670946c106f170a9018ca0cc/CaptureOne.Mac.#{version}.dmg"
   name "Capture One"
   homepage "https://www.captureone.com/"
