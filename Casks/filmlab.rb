a8b4c48ad6b9cd559dc132c1dc4a187185174513
MrBrutti <matias@section9labs.com>
2025-04-17 15:18:23 -0700

    Updating Filmlab to 3.1

--- Casks/filmlab.rb
+++ Casks/filmlab.rb
@@ -1,8 +1,8 @@
 cask "filmlab" do
-  version "3.0.0"
+  version "3.1.0"
   sha256 :no_check
 
-  url "https://downloads.filmlabapp.com/desktop/FilmLab-3.0.0-universal.dmg"
+  url "https://downloads.filmlabapp.com/desktop/FilmLab-3.1.0-universal.dmg"
   name "FilmLab"
   desc "Film photography scanning app"
   homepage "https://www.filmlabapp.com/"
