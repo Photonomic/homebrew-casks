0a5131642640e2e7d729d0357e2957a50da97713
MrBrutti <matias@section9labs.com>
2025-10-14 13:53:51 -0700

    Updating NLP to 3.1.1 v3 - Fixing .pkg filename issue

--- Casks/negative-lab-pro.rb
+++ Casks/negative-lab-pro.rb
@@ -2,7 +2,6 @@ cask "negative-lab-pro" do
   version "3.1.1"
   sha256 "2edcb8a1e3a08ba0cb1d9844e01988ed997284a28514db949ddb8b55aecfd9e5"
   
-
   url "https://negativelabpro.s3-accelerate.amazonaws.com/MAC-NEGATIVE-LAB-PRO-v3.1.1.zip",
       verified: "negativelabpro.s3-accelerate.amazonaws.com"
   name "Negative Lab Pro"
@@ -11,7 +10,7 @@ cask "negative-lab-pro" do
 
   # ARM Mac (Apple Silicon) requires Rosetta 2
   if Hardware::CPU.intel?
-    installer manual: "Install Negative Lab Pro.pkg"
+    installer manual: "Install Negative Lab Pro v#{version}.pkg"
   else
     depends_on arch: :arm64, macos: ">= :big_sur"
     postflight do
@@ -20,7 +19,7 @@ cask "negative-lab-pro" do
                      sudo: true
     end
 
-    installer manual: "Install Negative Lab Pro.pkg"
+    installer manual: "Install Negative Lab Pro v#{version}.pkg"
   end
 
   uninstall pkgutil: "com.negativelabpro.pkg" # Replace this if the identifier is different.
