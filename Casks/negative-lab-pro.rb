13976f922fc9cea92a08f298e40e748b25ece0ee
MrBrutti <matias@section9labs.com>
2025-10-14 15:20:34 -0700

    Updating NLP to 3.1.1 v4 - Fixing .pkg filename issue

--- Casks/negative-lab-pro.rb
+++ Casks/negative-lab-pro.rb
@@ -2,7 +2,7 @@ cask "negative-lab-pro" do
   version "3.1.1"
   sha256 "2edcb8a1e3a08ba0cb1d9844e01988ed997284a28514db949ddb8b55aecfd9e5"
   
-  url "https://negativelabpro.s3-accelerate.amazonaws.com/MAC-NEGATIVE-LAB-PRO-v3.1.1.zip",
+  url "https://negativelabpro.s3-accelerate.amazonaws.com/MAC-NEGATIVE-LAB-PRO-v#{version}.zip",
       verified: "negativelabpro.s3-accelerate.amazonaws.com"
   name "Negative Lab Pro"
   desc "Software for converting and editing scanned film negatives"
